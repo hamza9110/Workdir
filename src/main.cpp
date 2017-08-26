c3d0dd80191e3ac5edf17d9d6616abc2638d1922
hamza.taghouti <taghouti.hamza@gmail.com>
2017-08-26 22:58:53 +0200

    New commit for all

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello Git" << std::endl;
+	std::cout << "Hello Git !!" << std::endl;
+	int y = 0; // master
+	int x = 0; //release1
+	int t = 0; // master
 	return 0;
 }
